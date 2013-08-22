1b80ff7d7548828afc47f04acc6c98515346ebeb
Keith <keith.grieshaber@clickbank.com>
2013-08-21 20:16:25 -0600

    Remove old examples

--- js/parks.js
+++
@@ -1,9 +0,0 @@
-function changePg() {
-  window.sessionStorage.setItem('park', park);
-  $.mobile.changePage("/html/attractionList.html",{ reloadPage : true });
-}
-function getHref(aElem, park) {
-    //set the selected park in the session
-    window.sessionStorage.setItem('park', park);
-    aElem.href = "/html/attractionList.html";
-}
